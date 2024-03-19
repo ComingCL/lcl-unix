82df0ad266ecec5eb3d411e9e64bf485f6e505fe
changleliu <1328004790@qq..com>
2024-03-19 23:30:57 +0800

    learn: print 'X'

--- boot_sect_memory.asm
+++ boot_sect_memory.asm
@@ -1,37 +1,30 @@
-mov ah, 0x0e
+[org 0x7c00]
+;
+; A simple boot sector program that demonstrates addressing.
+;
+mov ah, 0x0e ; int 10/ ah = 0eh -> scrolling teletype BIOS routine
 
-; mov al, "1"
-; int 0x10
-; mov al, the_secret
-; int 0x10
+; First attempt
+mov al, the_secret
+int 0x10 ; Does this print an X?
 
-; mov al, "2"
-; int 0x10
-; mov al, the_secret
-; int 0x10
+; Second attempt
+mov al, [the_secret]
+int 0x10 ; Does this print an X?
 
-; mov al, "3"
-; int 0x10
-; mov bx, the_secret
-; add bx, 0x7c00
-; mov al, [bx]
-; int 0x10
-
-; mov al, "4"
-; int 0x10
-; mov al, [0x7c2d]
-; int 0x10
-
+; Third attempt
 mov bx, the_secret
 add bx, 0x7c00
 mov al, [bx]
-int 0x10
+int 0x10 ; Does this print an X?
 
-jmp $
+; Fourth attempt
+mov al, [0x7c1d]
+int 0x10 ; Does this print an X?
+jmp $ ; Jump forever.
 
 the_secret:
   db "X"
-
+; Padding and magic BIOS number.
 times 510-($-$$) db 0
-
 dw 0xaa55
